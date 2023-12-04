fc9644330f94af98d4d9563c554f60d4ece287d0
njwright92 <njwright92@gmail.com>
2023-12-04 13:33:11 -0800

    "testing new transcripts and training n gguf"

--- model_training.py
+++ model_training.py
@@ -28,22 +28,15 @@ def main():
 
     # Load and concatenate multiple training data files
     train_transcripts = load_transcripts(
-        'train.txt', 'train2.txt', 'train3.txt')
-
-    # Load and concatenate multiple testing data files
-    test_transcripts = load_transcripts('test.txt', 'test2.txt', 'test3.txt')
+        'train3.txt', 'combined_comedy_transcripts.txt')
 
     # Tokenize the data
     train_encodings = tokenizer(train_transcripts, truncation=True,
                                 padding='max_length', max_length=tokenizer.model_max_length)
-    test_encodings = tokenizer(test_transcripts, truncation=True,
-                               padding='max_length', max_length=tokenizer.model_max_length)
 
     # Create datasets
     train_dataset = Dataset.from_dict(
         {"input_ids": train_encodings["input_ids"], "attention_mask": train_encodings["attention_mask"]})
-    test_dataset = Dataset.from_dict(
-        {"input_ids": test_encodings["input_ids"], "attention_mask": test_encodings["attention_mask"]})
 
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False)
@@ -51,17 +44,14 @@ def main():
     training_args = TrainingArguments(
         output_dir="./model",
         overwrite_output_dir=True,
-        num_train_epochs=4,
+        num_train_epochs=3,
         per_device_train_batch_size=4,
         gradient_accumulation_steps=4,
         save_steps=2_000,
         save_total_limit=2,
         logging_dir='./logs',
-        learning_rate=5e-5,
+        learning_rate=1e-4,
         weight_decay=0.01,
-        evaluation_strategy="steps",
-        eval_steps=500,
-        warmup_steps=300
     )
 
     trainer = Trainer(
@@ -69,12 +59,10 @@ def main():
         args=training_args,
         data_collator=data_collator,
         train_dataset=train_dataset,
-        eval_dataset=test_dataset,
     )
 
-    # Training and evaluation
+    # Training
     trainer.train()
-    trainer.evaluate()
 
     # Saving the trained model
     model.save_pretrained("./model")
--- run_gguf.py
+++ run_gguf.py
@@ -1,18 +1,17 @@
 from llama_cpp import Llama
 
-MODEL_PATH = 'mistral-comedy-2.0-ckpt-600.Q6_K.gguf'
+MODEL_PATH = 'mistral-comedy-3.0-ckpt-1600.Q6_K.gguf'
 model = Llama(model_path=MODEL_PATH)
 
 while True:
-    prompt = input('COMPLETION PROMPT: ')
+    prompt = input('PROMPT: ')
     output = model(
         prompt,
-        max_tokens=256,
+        max_tokens=500,
         echo=False,
         stream=True,
     )
-    print()
-    print(prompt, end='')
+
+    print(end='')
     for x in output:
         print(x['choices'][0]['text'], end='', flush=True)
-    print()
